1aa4a8f1e5ee751b285fc9e8970e195f82b92e41
Karen Attfield <k_attfield@yahoo.com>
2016-10-20 09:15:54 +0100

    Redesigning responsive menu

--- header.php
+++ header.php
@@ -1,32 +1,32 @@
-
 <!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title('|', true, 'right'); ?></title>
+<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="author" content="">
 
 	<!-- Mobile specification Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 	<!-- Stylesheets -->
 	<link href='https://fonts.googleapis.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 	
 	<link rel="stylesheet/less" href="css/styles.less" type="text/css" />
-	<link rel="stylesheet/less" type="text/css" href="styles.less">
-<script src="js/jquery-1.11.3.min.js"></script>
-<script src="less.js" type="text/javascript"></script>
-<script type="text/javascript">
-  less.watch();
-</script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<script src="js/scripts.js" type="text/javascript"></script>
 	
-	
+
+
 
 
 	<!--[if lt IE 9]>
@@ -44,12 +44,9 @@
 
 </head>
 
-<body <?php body_class(); ?>>
+<body>
 
 <!-- HEADER -->
-<?php $options = get_option('k1_custom_settings'); ?>
-<?php $logo = isset($options['logo']) ? $options['logo'] : ''; ?>
-
 	
 	<header class="main-header" id="top">
 
@@ -61,11 +58,8 @@
 				<div class="col-lg-3 logo-container">
 
 					<h1 class="logo">
-						<?php
-					
-						isset($options['logo']) == '' ? $logo = IMAGES . '/logo.png' : $logo = $options['logo']; ?>
 						<a href="<?php echo home_url(); ?>">
-						<img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?> | <?php bloginfo('description'); ?>" />
+						<img src="<?php print IMAGES; ?>/logo.png" alt="<?php bloginfo('name'); ?> | <?php bloginfo('description'); ?>" />
 					</a>
 					</h1> 
 
@@ -73,21 +67,27 @@
 
 				</div> <!-- end row -->
 
-	<nav class="main-navigation clearfix">
+	<nav class="main-navigation responsive-navigation clearfix">
 
+			<div id="responsive-menu-icon">
+				<div class="hamburger"></div>
+				<div class="hamburger"></div>
+				<div class="hamburger"></div>
+			Menu </div>
 			<?php wp_nav_menu(array(
 				'theme_location' => 'main-menu'
 			)); ?>
 
 			</nav>
 			
-			<a href="#" class="small-button green" id="rwd-main-nav-btn"><?php _e('Select a page...', 'k1-framework'); ?></a>
-			<div class="rwd-main-nav"></div> <!-- end rwd-main-nav -->
+
 
 			
 			</div> <!-- end container -->
 
 		</div> <!-- end top-menu-container -->
+
+
 	
 
 
